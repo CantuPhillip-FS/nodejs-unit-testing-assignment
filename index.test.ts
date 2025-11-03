b6045bb48a5a421b3d174683879189b2b294f2d2
hereisphil <thereisphil@gmail.com>
2025-11-02 21:20:17 -0600

    Initial commit, includes: full Typescript + Jest setup, initial Basic Math tests in index.tst.ts and first add function in index.ts

---
+++ index.test.ts
@@ -0,0 +1,22 @@
+const { add, subtract, divide, multipy } = require("./index");
+
+/* -------------------------------------------------------------------------- */
+/*                         1st Test Suite: Basic Math                         */
+/* -------------------------------------------------------------------------- */
+describe("Testing Basic math operations", () => {
+  test("two numbers are added and a sum is returned", () => {
+    expect(add(3, 7)).toBe(10);
+  });
+
+  test("two numbers are subtracted and a difference is returned", () => {
+    expect(subtract(12, 6)).toBe(6);
+  });
+
+  test("two numbers are divided and a quotient is returned", () => {
+    expect(divide(100, 50).toBe(2));
+  });
+
+  test("two numbers are multipled and a product is returned", () => {
+    expect(multipy(3, 9).toBe(27));
+  });
+});
---
+++ index.ts
@@ -0,0 +1,3 @@
+const add = (numOne: number, numTwo: number): number => numOne + numTwo;
+
+module.exports = { add, subtract, divide, multiply };
